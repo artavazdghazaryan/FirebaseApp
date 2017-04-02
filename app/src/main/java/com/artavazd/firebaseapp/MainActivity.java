7f21936ad6f140c9aad731b579a37e097fa6df78
Artavazd Ghazaryan <artavazd.ghazaryan@gmail.com>
2017-04-02 02:53:17 -0700

    Cloud Storage initial branch with UI only, non functional

--- app/src/main/java/com/artavazd/firebaseapp/MainActivity.java
+++ app/src/main/java/com/artavazd/firebaseapp/MainActivity.java
@@ -20,11 +20,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends BaseActivity {
     private static final String TAG = MainActivity.class.getName();
@@ -113,6 +115,29 @@ public class MainActivity extends BaseActivity {
                         // ...
                     }
                 });
+    }
+
+    private void receiveFromCloud(StorageReference reference) {
+        try {
+            File localFile = File.createTempFile("images", "jpg");
+            reference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            // Successfully downloaded data to local file
+                            // ...
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle failed download
+                    // ...
+                }
+            });
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     private boolean validateMessage() {
