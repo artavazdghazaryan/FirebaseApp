c1c380d6d0c5ed9a51507b650f61e7c4b02bbb32
Artavazd Ghazaryan <artavazd.ghazaryan@gmail.com>
2017-04-02 02:46:10 -0700

    Cloud Storage initial branch with UI only, non functional

--- app/src/main/java/com/artavazd/firebaseapp/MainActivity.java
+++ app/src/main/java/com/artavazd/firebaseapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.artavazd.firebaseapp;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -15,15 +20,27 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class MainActivity extends BaseActivity {
     private static final String TAG = MainActivity.class.getName();
 
     public static String EXTRA_USER_NAME = "username";
+
+    //Firebase Realtime Database UI elements
     private TextView tv1, tv2;
     private TextView tvBoard;
     private EditText etMessage;
     private Button bSendMessage;
+
+    //Firebase Cloud Storage UI elements
+
+    private Button bSendToCloud;
+    private ImageView ivCloudImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,46 +65,79 @@ public class MainActivity extends BaseActivity {
                 finish();
             }
         });
-        //RealTime Database UI
+        //RealTime Database UI & Firebase variable initiation
         database = FirebaseDatabase.getInstance();
-        etMessage=(EditText)findViewById(R.id.et_message);
+        etMessage = (EditText) findViewById(R.id.et_message);
 
-        tvBoard=(TextView)findViewById(R.id.tv_board);
+        tvBoard = (TextView) findViewById(R.id.tv_board);
         bindTVBoardToFirebaseChild("message");
-        bSendMessage=(Button)findViewById(R.id.b_send_message);
+        bSendMessage = (Button) findViewById(R.id.b_send_message);
         bSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!validateMessage()){
+                if (!validateMessage()) {
                     return;
                 }
                 sendMessage("message", etMessage.getText().toString());
             }
         });
-        }
+        //Storage UI & Firebase variable initiation
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        ivCloudImage = (ImageView) findViewById(R.id.ivCloudImage);
+        bSendToCloud = (Button) findViewById(R.id.b_send_to_cloud);
+        bSendToCloud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToCloud();
+            }
+        });
+
+    }
+
+    private void sendToCloud() {
+        Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
+        StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
+
+        riversRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
 
     private boolean validateMessage() {
 
-            boolean result = true;
-            if (TextUtils.isEmpty(etMessage.getText().toString())) {
-                etMessage.setError("Required");
-                result = false;
-            } else {
-                etMessage.setError(null);
-            }
-
-            if ((etMessage.getText().toString()).length()<3) {
-                etMessage.setError("Too short");
-                result = false;
-            } else {
-                etMessage.setError(null);
-            }
-
-            return result;
+        boolean result = true;
+        if (TextUtils.isEmpty(etMessage.getText().toString())) {
+            etMessage.setError("Required");
+            result = false;
+        } else {
+            etMessage.setError(null);
         }
 
-   private FirebaseDatabase database;
-   private DatabaseReference myRef;
+        if ((etMessage.getText().toString()).length() < 3) {
+            etMessage.setError("Too short");
+            result = false;
+        } else {
+            etMessage.setError(null);
+        }
+
+        return result;
+    }
+
+    //Firebase Database variables and methods
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     private void sendMessage(String reference,
                              String message) {
@@ -97,7 +147,7 @@ public class MainActivity extends BaseActivity {
         myRef.push().setValue(message);
     }
 
-    private void bindTVBoardToFirebaseChild(String reference){
+    private void bindTVBoardToFirebaseChild(String reference) {
         // Read from the database
         myRef = database.getReference(reference);
         ChildEventListener childEventListener = new ChildEventListener() {
@@ -110,7 +160,7 @@ public class MainActivity extends BaseActivity {
 
                 // [START_EXCLUDE]
                 // Update RecyclerView
-                tvBoard.append(message+"\n");
+                tvBoard.append(message + "\n");
                 // [END_EXCLUDE]
             }
 
@@ -150,5 +200,6 @@ public class MainActivity extends BaseActivity {
         myRef.addChildEventListener(childEventListener);
     }
 
-
+    //Firebase Storage variables and methods
+    private StorageReference mStorageRef;
 }
